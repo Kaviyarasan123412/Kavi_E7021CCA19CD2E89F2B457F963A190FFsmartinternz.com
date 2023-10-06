91f26ec41cce10f02dbe2c29b2577b5aabe0754f
Kaviyarasan123412 <144423970+Kaviyarasan123412@users.noreply.github.com>
2023-10-06 09:40:43 +0530

    Update challange 2.2.py

--- challange 2.2.py
+++ challange 2.2.py
@@ -1,43 +1,15 @@
-class Player(object):
-    def __init__(self, name, age, skills, style=None):
-        self.name = name
-        self.age = age
-        self.skills = skills
-        self.style = style
+class player:
+  def play(self):
+    print("the player is playing cricket")
+class batsman(player):
+  def play(self):
+    print("the batsman is batting")
+class bowler(player):
+  def play(self):
+    print("the bowler is bowling")
+batsman=batsman()
+bowler =bowler()
+batsman.play()
+bowler.play()
 
-    def get_player(self):
-        print(self.name,self.age,self.skills,self.style)
-
-
-class Team(object):
-    def __init__(self, name):
-        self.name = name
-        self._players = []
-
-    def add_player(self, obj):
-        if isinstance(obj, Player):
-            self._players.append(obj)
-        else:
-            print("Please provide player object")
-
-    def get_players(self):
-        for player in self._players:
-            player.get_player()
-
-
-if __name__ == "__main__":
-
-    p1 = Player("Mahendra", 46, "Wicket Kipper", "Right-Hand Batsman")
-    p2 = Player("Sachin", 35, "Batsman", "Right-Hand Batsman")
-    p3 = Player("Saurabh", 44, "Batsman", "Left-Hand Batsman")
-    p4 = Player("Zahir", 38, "Bauwller", "Medium Pace Bauwller")
-    p5 = Player("Yuvraj", 43, "All rounder")
-
-    t = Team("India")
-    t.add_player(p1)
-    t.add_player(p2)
-    t.add_player(p3)
-    t.add_player(p4)
-    t.add_player(p5)
-
-    t.get_players()
+    
